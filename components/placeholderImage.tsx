1958481f464997e26ff48ba3d037b4cfd2c73ce7
Rosie <Xavier.r1990@gmail.com>
2022-08-30 21:02:36 -0400

    image changes

--- components/placeholderImage.tsx
+++ components/placeholderImage.tsx
@@ -4,7 +4,7 @@ import { idList } from '../constants/imageList';
 
 export const JunglersImage: FC<{Id: any}> = (props) => (
     <motion.div whileHover={{scale: 1.05}} className="junglers-container">
-        <img src={`https://d3ujpwzi55x5a1.cloudfront.net/${idList[props.Id]}.gif`} className="rounded-t"/>
+        <img src={`https://d3ujpwzi55x5a1.cloudfront.net/${idList[props.Id]}`} className="rounded-t"/>
         <div className="junglers-container-subtitle">
             Jungler #{props.Id}
         </div>
