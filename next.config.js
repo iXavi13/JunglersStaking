42ecd72e42bb52acfc6ed3bd233687dcac012461
Rosie <Xavier.r1990@gmail.com>
2022-09-05 14:07:49 -0400

    ignore electron

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin({
+        resourceRegExp: /^electron$/
+    }));
+    return config;
+  },
 }
 
 module.exports = nextConfig
